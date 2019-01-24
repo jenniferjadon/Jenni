42883fd4517424f213142af329a271a52924a5a6
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-01-24 16:28:08 +0530

    Create numwords6.py

---
+++ numwords6.py
@@ -0,0 +1,27 @@
+num=int(input())
+if num == 1:
+    print("One")
+if num == 2:
+    print("Two")
+if num == 3:
+    print("Three")
+if num == 4:
+    print("Four")
+if num == 5:
+    print("Five")
+if num == 6:
+    print("Six")
+if num == 7:
+    print("Seven")
+if num == 8:
+    print("Eight")
+if num == 9:
+    print("Nine")
+if num == 10:
+    print("Ten")
+
+    
+    
+    
+        
+    
