10660fef267a5e84c6c4318570dff5ce58e8c8d7
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-01-31 22:44:08 +0530

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+c=[int(i) for i in input().split()]
+d=0
+for r in range(0,len(c)):
+    if c[r]==b:
+        print("yes")
+        break
+else:
+    print("no")
+    
