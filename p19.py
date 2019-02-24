a47d416a0378ce5078763494ed77515c1d19f6a6
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-02-24 16:30:50 +0530

    Create p19.py

---
+++ p19.py
@@ -0,0 +1,22 @@
+
+def isprime(m):
+    c=0
+    for j in range(2,m):
+        if m%j==0:
+            c=c+1
+    if c==0:
+        return 1
+    else:
+        return 0
+
+n=int(input())
+count=0
+for i in range(2,n+1):
+    if n%i==0:
+        if count==0:
+            if isprime(i)==1:
+                print(i,end="")
+                count+=1
+        else:
+            if isprime(i)==1:
+print("",i,end="")
