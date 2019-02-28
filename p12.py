e4d0b62ea1f36cc134b99d420921414c1937be2f
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-02-28 08:23:36 +0530

    Create p12.py

---
+++ p12.py
@@ -0,0 +1,13 @@
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+c=0
+for i in range(0,k):
+    a.insert(0,a[-1])
+    del(a[-1])
+for j in range(len(a)):
+    if c==0:
+        print(a[j],end='')
+        c=c+1
+    else:
+        print("",a[j],end='')
+
