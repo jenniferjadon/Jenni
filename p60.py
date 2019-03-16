afd8de58156a551e98d7e7af1513076a0f7ee3ad
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-03-16 14:03:40 +0530

    Create p60.py

---
+++ p60.py
@@ -0,0 +1,9 @@
+n,m=map(str,input().split())
+for i in range (0,len(n)):
+    for i in range (0,len(m)):
+        if n[i]==m[i]:
+            print("yes")
+            break
+            break
+else:
+    print("no")
