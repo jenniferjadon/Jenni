3d1904e04ae1436ebdf4d23f7db424dc3f75ea1e
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-03-18 16:50:08 +0530

    Update p82.py

--- p82.py
+++ p82.py
@@ -3,9 +3,11 @@ h=list(map(int,input().split()))
 for i in range(0,len(h)):
     if n==1:
         print(h[i])
+        break
     elif h[i]==h[i+1]:
         print(h[i])
         break
     else:
         print('0')
+        break
         
