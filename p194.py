ebe14d16cd00cabe818d2add564cb8b5662475df
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-05-15 03:01:13 -0700

    Create p194.py

---
+++ p194.py
@@ -0,0 +1,19 @@
+n,m=input().split()
+if n=='R' and m=='P':
+    print("P")
+if n=='S' and m=='P':
+    print("S")
+if n=='R' and m=='S':
+    print("R")
+if n=='P' and m=='R':
+    print("P")
+if n=='P' and m=='S':
+    print("S")
+if n=='S' and m=='R':
+    print("R")
+if n=='P' and m=='P':
+    print("D")
+if n=='S' and m=='S':
+    print("D")
+if n=='R' and m=='R':
+    print("D")
