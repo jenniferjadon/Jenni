1c7951bbb18ab7d2b35265eea767dc77e29b142a
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-06-05 11:59:46 +0530

    Update p206.py

--- p206.py
+++ p206.py
@@ -1,1 +1,11 @@
-
+p=input()
+c=0
+g=[1-9]
+for i in p:
+    if i==g or i=='A' or i=='B' or i=='C' or i=='D' or i=='E' or i=='F':
+        c=c+1 
+if c==len(p):
+    print("yes")
+else:
+    print("no")
+        
