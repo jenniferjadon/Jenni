0d2ad309015997927a6ecf5f658de82550df8e69
jenniferjadon <46442323+jenniferjadon@users.noreply.github.com>
2019-06-06 11:46:10 +0530

    Create p94.py

---
+++ p94.py
@@ -0,0 +1,11 @@
+k=int(input())
+m=list(str(k))
+c=0
+for i in range(0,len(m)):
+    for j in range(1,len(m)):
+        if m[i]==m[j]:
+            c=c+1 
+if c>0:
+    print("yes")
+else:
+    print("no")
